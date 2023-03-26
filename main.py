2b02e7a3c37965cd6299a2ce7beb8aa614d348af
Denys Morozov <ajax3101@gmail.com>
2023-03-26 20:20:40 +0300

    corrected headers

--- main.py
+++ main.py
@@ -1,33 +1,31 @@
+import os
 import requests
 from tqdm import tqdm
 from dotenv import load_dotenv
 import json
 import math
-import os
+
 
 
 load_dotenv()
 
 
-def scrap_pexels(query=""):
-    """Your API Key
-    Keyword arguments:
-    argument -- description
-    Return: return_description
-    """
+def scrap_pexels(query=''):
+    """Your API Key"""
 
-    headers = {"Authorization:" f'{os.getenv("API_KEY")}'}
-    query_str = f"https://api.pexels.com/v1/search?query={query}&per_page=40&orientation=landscape"
+    headers = {"Authorization": f'{os.getenv("API_KEY")}'}
+    print(headers)
+    query_str = f'https://api.pexels.com/v1/search?query={query}&per_page=40&orientation=landscape'
 
-    response = requests.get(url=query_str, headers=headers)
+    response = requests.get(url=query_str, headers=headers, proxies=None)
 
     if response.status_code != 200:
-        return f"Ошибка: Статус кода - {response.status_code}, {response.json()}"
+        return f'Ошибка: Статус кода - {response.status_code}, {response.json()}'
 
 
 def main():
-    scrap_pexels(query="hacker")
+    scrap_pexels(query='parsing')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
